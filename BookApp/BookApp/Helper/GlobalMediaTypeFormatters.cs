c88ed00ee530e78c41a00649f51e72533585f077
Fadi Moussa <pactwiththedevil@gmail.com>
2021-06-06 16:30:52 +0200

    Added GlobalMediaTypeFormatters

---
+++ BookApp/BookApp/Helper/GlobalMediaTypeFormatters.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+
+namespace BookApp
+{
+
+    public class GlobalMediaTypeFormatters
+    {
+
+        /* This class is used to format the output response and enables developers to add different formats in a central place for easy maintainability */
+
+        public JsonMediaTypeFormatter JsonFormatter
+        {
+            get
+            {
+                var formatter = new JsonMediaTypeFormatter();
+                var json = formatter.SerializerSettings;
+
+                json.DateFormatHandling = Newtonsoft.Json.DateFormatHandling.IsoDateFormat;
+                json.DateTimeZoneHandling = Newtonsoft.Json.DateTimeZoneHandling.Utc;
+                json.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
+                json.Formatting = Newtonsoft.Json.Formatting.Indented;
+                json.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                json.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                return formatter;
+            }
+        }
+    }
+}
