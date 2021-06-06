a0c4d1c9a8d7ead515b0213e1d6836760acee9aa
Fadi Moussa <pactwiththedevil@gmail.com>
2021-06-06 17:27:49 +0200

    Added BookBase as base class for book and updated models Book and BookExtended to use it.

---
+++ BookApp/Models/BookBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/* This class contains the base elements of Book and BookExtended. */
+
+namespace Models
+{
+    public class BookBase
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedDatetime { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
--- BookApp/Models/DomainModels/Book.cs
+++ BookApp/Models/DomainModels/Book.cs
@@ -4,14 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Models.DomainModels {
-    public class Book {
-        public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Content { get; set; }
-        public DateTime CreatedDatetime { get; set; }
-        public Guid UserId { get; set; }
+namespace Models.DomainModels
+{
+    public class Book : BookBase
+    {
         public User User { get; set; }
     }
 }
