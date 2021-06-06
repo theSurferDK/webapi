5c439e341895e53ced7ec99ac36946f34e4dfaf0
Fadi Moussa <pactwiththedevil@gmail.com>
2021-06-06 16:33:17 +0200

    Added DataTransferObjects

---
+++ BookApp/BookApp/DataTransferObjects/BookDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookApp.DataTransferObjects
+{
+		public class BookDTO
+		{
+				public Guid Id { get; set; }
+				public string Title { get; set; }
+				public string Author { get; set; }
+
+		}
+}
